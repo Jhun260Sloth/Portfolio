bfcc3206ba1b09437d934b4e42a580ba18dd5637
Jhunz_260 <99332717+Jhun260Sloth@users.noreply.github.com>
2022-08-21 19:37:44 +0800

    Add files via upload

--- body.js
+++ body.js
@@ -6,37 +6,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
-var myIndex = 0;
-var myIndex2 = 0;
-carousel();
-carousel2();
-
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("UIhigh");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
-
-
-function carousel2() {
-  var i;
-  var x = document.getElementsByClassName("UIhigh2");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex2++;
-  if (myIndex2 > x.length) {myIndex2 = 1}    
-  x[myIndex2-1].style.display = "block";  
-  setTimeout(carousel2, 2000); // Change image every 2 seconds
-}
-
-
-
